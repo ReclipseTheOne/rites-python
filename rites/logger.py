74b1aa8e3ff2f292fc55ea79f178fedc01f6ae3f
Reclipse <17797013+ReclipseTheOne@users.noreply.github.com>
2025-04-14 19:53:20 +0300

    0.4.2 - Logger improvements

--- rites/logger.py
+++ rites/logger.py
@@ -33,32 +33,16 @@ class Logger:
             log_path (str): The path to the log file directory
             log_filename_format (str): The format of the log file name - default (log-%Y-%m-%d-%Hh-%Mm-%Ss)
             log_inline_format (str): The format for the time stamp on the log lines - default ([%Y-%m-%d %H:%M:%S])
+            handles_zipping (bool): Only edit this variable if there are multiple loggers in the same folder
     """
-    def __init__(self, log_path, max_logs=5, log_name="Unnamed Logger", log_level=2, log_console_format="%Y-%m-%d %H:%M:%S"):
+    def __init__(self, log_path, max_logs=5, log_name="Unnamed Logger", log_level=2, log_console_format="%Y-%m-%d %H:%M:%S", handles_zipping=True, is_secondary=False):
         self.printer = Printer()
         self.log_path = log_path
         self.max_logs = max_logs
         self.name = log_name
         self.log_inline_format = log_console_format
-
-        # Remove the oldest log if it exceeds max_logs
-        oldest_log = f"{self.log_path}/log_{self.max_logs}.rar"
-        if os.path.exists(oldest_log):
-            os.remove(oldest_log)
-
-        # Shift log files
-        for i in range(self.max_logs - 1, -1, -1):
-            current_log = f"{self.log_path}/log_{i}.rar"
-            next_log = f"{self.log_path}/log_{i + 1}.rar"
-            if os.path.exists(current_log):
-                os.rename(current_log, next_log)
-
-        # Compress latest.log into log_0.rar
-        latest_log = f"{self.log_path}/latest.log"
-        if os.path.exists(latest_log):
-            with zipfile.ZipFile(f"{self.log_path}/log_0.rar", 'w', zipfile.ZIP_DEFLATED) as log_archive:
-                log_archive.write(latest_log, arcname="latest.log")
-            os.remove(latest_log)
+        self.handles_zipping = handles_zipping
+        self.is_secondary = is_secondary
 
         atexit.register(self._exit_handler)
 
@@ -80,11 +64,33 @@ class Logger:
 
     def _exit_handler(self):
         exc_type, exc_value, exc_traceback = sys.exc_info()
-        if exc_type is None:
-            self.success("Program exited successfully.")
-        else:
-            self.error("Program exited with an error.")
-            self.error("".join(traceback.format_exception(exc_type, exc_value, exc_traceback)))
+
+        if self.is_secondary is not True:
+            if exc_type is None:
+                self.success("Program exited successfully.")
+            else:
+                self.error("Program exited with an error.")
+                self.error("".join(traceback.format_exception(exc_type, exc_value, exc_traceback)))
+
+        if self.handles_zipping is True:
+            # Remove the oldest log if it exceeds max_logs
+            oldest_log = f"{self.log_path}/log_{self.max_logs}.rar"
+            if os.path.exists(oldest_log):
+                os.remove(oldest_log)
+
+            # Shift log files
+            for i in range(self.max_logs - 1, -1, -1):
+                current_log = f"{self.log_path}/log_{i}.rar"
+                next_log = f"{self.log_path}/log_{i + 1}.rar"
+                if os.path.exists(current_log):
+                    os.rename(current_log, next_log)
+
+            # Compress latest.log into log_0.rar
+            latest_log = f"{self.log_path}/latest.log"
+            if os.path.exists(latest_log):
+                with zipfile.ZipFile(f"{self.log_path}/log_0.rar", 'w', zipfile.ZIP_DEFLATED) as log_archive:
+                    log_archive.write(latest_log, arcname="latest.log")
+                os.remove(latest_log)
         
     def printable_timestamp(self) -> str:
         """ Returns a colored console printable timestamp
@@ -101,6 +107,9 @@ class Logger:
             str: The formatted name
         """
         return f"{self.printer.get_color("gray")}{self.name}{Style.reset}"
+
+    def add_custom(self, key: str, word: str, r: int, g: int, b: int):
+        self.printer.add_style(key, word, r, g, b)
 
     def custom(self, style_key: str, *txt):
         """ Logs a custom styled message
@@ -158,3 +167,11 @@ class Logger:
                 txt (str): The message to log
         """
         self.custom("info", *txt)
+
+
+def get_logger(log_path, max_logs=5, log_name="Unnamed Logger", log_level=2, log_console_format="%Y-%m-%d %H:%M:%S", handles_zipping=True):
+    return Logger(log_path, max_logs, log_name, log_level, log_console_format, handles_zipping)
+
+
+def get_sec_logger(log_path, log_name="Unnamed Secondary Logger", log_level=2, log_console_format="%Y-%m-%d %H:%M:%S"):
+    return Logger(log_path, log_name=log_name, log_level=log_level, log_console_format=log_console_format, handles_zipping=False, is_secondary=True)
