359817f8aabdf8630ca0ee96ccdda74cf4eac365
Reclipse <17797013+ReclipseTheOne@users.noreply.github.com>
2025-08-18 12:51:41 +0200

    0.5.1 - Logger Improvements

--- rites/logger/logger.py
+++ rites/logger/logger.py
@@ -22,20 +22,20 @@ class Logger:
             log_inline_format (str): The format for the time stamp on the log lines - default ([%Y-%m-%d %H:%M:%S])
             handles_zipping (bool): Only edit this variable if there are multiple loggers in the same folder
     """
-    def __init__(self, log_path, max_logs=5, log_name="Unnamed Logger", log_level=2, log_console_format="%Y-%m-%d %H:%M:%S", handles_zipping=True, is_secondary=False):
+    def __init__(self, log_path, max_logs=5, log_name="Unnamed Logger", log_level=2, log_console_format="%Y-%m-%d %H:%M:%S", handles_zipping=True, handles_exit_message=False):
         self.printer = Printer()
         self.log_path = log_path
         self.max_logs = max_logs
         self.name = log_name
         self.log_inline_format = log_console_format
+
+        # Attributes
         self.handles_zipping = handles_zipping
-        self.is_secondary = is_secondary
+        self.handles_exit_message = handles_exit_message
+        self.should_log_to_file = True;
+        self.should_print_to_console = True;
 
         atexit.register(self._exit_handler)
-
-    def _get_filename_formatted_timestamp(self):
-        now = datetime.now()
-        return now.strftime(self.log_filename_format)
 
     def _get_inline_formatted_timestamp(self):
         now = datetime.now()
@@ -52,7 +52,7 @@ class Logger:
     def _exit_handler(self):
         exc_type, exc_value, exc_traceback = sys.exc_info()
 
-        if self.is_secondary is not True:
+        if self.handles_exit_message is not True:
             if exc_type is None:
                 self.success("Program exited successfully.")
             else:
@@ -112,8 +112,12 @@ class Logger:
             # For multiple arguments, join them with spaces
             string = " ".join(str(item) for item in txt)
 
-        print(f"{self.printable_timestamp()} [{self.formatted_name()}] {self.printer.get_style(style_key).get_str()} {string}")
-        self._writeToLogFile(f"[{self._get_inline_formatted_timestamp()}] [{self.name}] {self.printer.get_style(style_key).get_simple_string()} {string}\n")
+        msg =  f"{self.printable_timestamp()} [{self.formatted_name()}] {self.printer.get_style(style_key).get_str()} {string}"
+        
+        if self.should_print_to_console:
+            print(msg)
+        if self.should_log_to_file:
+            self._writeToLogFile(msg)
 
     def warning(self, *txt):
         """ Logs a warning message
@@ -155,10 +159,43 @@ class Logger:
         """
         self.custom("info", *txt)
 
+    def should_print(self, should_print: bool) -> "Logger":
+        """ Sets whether the logger should print to console"""
+        self.should_print_to_console = should_print
+        return self
+
+    def should_log(self, should_log: bool) -> "Logger":
+        """ Sets whether the logger should log to file"""
+        self.should_log_to_file = should_log
+        return self
+    
+    def dont_show_exit_message(self) -> "Logger":
+        """ Disables the exit message that is printed when the program exits"""
+        self.handles_exit_message = False
+        return self
+    
+    def disable(self) -> "Logger":
+        """ Disables the logger from logging to file and printing to console"""
+        self.should_print_to_console = False
+        self.should_log_to_file = False
+        return self
+    
+    def enable(self) -> "Logger":
+        """ Enables the logger to log to file and print to console"""
+        self.should_print_to_console = True
+        self.should_log_to_file = True
+        return self
 
 def get_logger(log_path, max_logs=5, log_name="Unnamed Logger", log_level=2, log_console_format="%Y-%m-%d %H:%M:%S", handles_zipping=True):
+    """Returns a Logger instance"""
     return Logger(log_path, max_logs, log_name, log_level, log_console_format, handles_zipping)
 
 
 def get_sec_logger(log_path, log_name="Unnamed Secondary Logger", log_level=2, log_console_format="%Y-%m-%d %H:%M:%S"):
-    return Logger(log_path, log_name=log_name, log_level=log_level, log_console_format=log_console_format, handles_zipping=False, is_secondary=True)
+    """Returns a Logger instance without zipping enabled"""
+    return Logger(log_path, log_name=log_name, log_level=log_level, log_console_format=log_console_format, handles_zipping=False, handles_exit_message=True)
+
+
+def get_tertiary_logger(log_name="Unnamed Tertiary Logger", log_level=2, log_console_format="%Y-%m-%d %H:%M:%S"):
+    """Returns a Logger instance without logging to file"""
+    return Logger("", log_name=log_name, log_level=log_level, log_console_format=log_console_format, handles_zipping=False, handles_exit_message=True)
